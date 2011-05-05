9c219f4e242aa5ee83bee8775abe72a7cc02cf29
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-05-05 23:43:18 +0000

    change to schema - dropping wl_wiki since its not necessary, changing wl_subject from blob to varchar for query performance

--- patches/WikiLoveLog.sql
+++ patches/WikiLoveLog.sql
@@ -8,10 +8,9 @@ CREATE TABLE IF NOT EXISTS /*_*/wikilove_log (
 	`wl_timestamp` binary(14) NOT NULL,
 	`wl_sender_id` int(11) NOT NULL,
 	`wl_receiver_id` int(11) NOT NULL,
-	`wl_wiki` varchar(64) NOT NULL,
 	`wl_type` varchar(64) NOT NULL,
 	`wl_template` varchar(64) NOT NULL,
-	`wl_subject` blob NOT NULL,
+	`wl_subject` varchar(255) NOT NULL,
 	`wl_message` blob NOT NULL,
 	`wl_email` bool NOT NULL default '0'
 ) /*$wgDBTableOptions*/;
