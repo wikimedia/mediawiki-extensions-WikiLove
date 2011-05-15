d68214532dbe38b04862b052ff05097e88298aee
Raimond Spekking <raymond@users.mediawiki.org>
2011-05-15 20:44:40 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-15 19:54:00 UTC)

--- WikiLove.i18n.php
+++ WikiLove.i18n.php
@@ -38,3 +38,234 @@ $messages['en'] = array(
 	'wikilove-err-sig' => 'Please do not include a signature in the message.',
 );
 
+/** Message documentation (Message documentation)
+ * @author McDutchie
+ */
+$messages['qqq'] = array(
+	'wikilove-button-preview' => 'Button text. Verb.',
+	'wikilove-preview' => 'Title. Noun.',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'wikilove-desc' => 'Ergänzt ein Hilfsmittel zum Ausdrücken persönlicher Wertschätzung gegenüber einem Benutzer auf dessen Diskussionsseite',
+	'wikilove' => 'Wikiliebe',
+	'wikilove-enable-preference' => 'Menüreiter für das Hilfsmittel zum Ausdrücken persönlicher Wertschätzung gegenüber einem anderen Benutzer aktivieren (experimentell)',
+	'wikilove-tab-text' => 'Wertschätzen',
+	'tooltip-ca-wikilove' => 'Diesem Benutzer die persönliche Wertschätzung in Form einer Nachricht hinterlassen',
+	'wikilove-dialog-title' => 'Persönliche Wertschätzung',
+	'wikilove-select-type' => 'Art auswählen',
+	'wikilove-get-started-header' => 'Los geht’s!',
+	'wikilove-get-started-list-1' => 'Die Art der persönlichen Wertschätzung auswählen, die ausgedrückt werden soll',
+	'wikilove-get-started-list-2' => 'Persönliche Wertschätzung ergänzen',
+	'wikilove-get-started-list-3' => 'Persönliche Wertschätzung senden',
+	'wikilove-add-details' => 'Ergänzungen hinzufügen',
+	'wikilove-title' => 'Titel:',
+	'wikilove-enter-message' => 'Eine Nachricht erfassen:',
+	'wikilove-omit-sig' => '(ohne Signatur)',
+	'wikilove-button-preview' => 'Vorschau',
+	'wikilove-preview' => 'Vorschau',
+	'wikilove-button-send' => 'Persönliche Wertschätzung senden',
+	'wikilove-type-makeyourown' => 'Eigene Art der Wertschätzung erstellen',
+);
+
+/** French (Français)
+ * @author Crochet.david
+ */
+$messages['fr'] = array(
+	'wikilove-tab-text' => 'Montrer l’appréciation',
+	'wikilove-select-type' => 'Sélectionnez le type',
+	'wikilove-add-details' => 'Ajouter des détails',
+	'wikilove-title' => 'Titre :',
+	'wikilove-enter-message' => 'Entrez un message :',
+	'wikilove-omit-sig' => '(sans signature)',
+	'wikilove-button-preview' => 'Prévisualiser',
+	'wikilove-preview' => 'Prévisualiser',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'wikilove-desc' => 'Engade unha interface para facilitar a mostra de aprecio cara a un usuario na súa páxina de conversa',
+	'wikilove' => 'Amor wiki',
+	'wikilove-enable-preference' => 'Activar o envío de mostras de aprecio aos outros usuarios mediante a lapela de amor wiki (experimental)',
+	'wikilove-tab-text' => 'Mostrar o aprecio',
+	'tooltip-ca-wikilove' => 'Déixelle unha mensaxe a este usuario mostrando o seu aprecio',
+	'wikilove-dialog-title' => 'Amor wiki',
+	'wikilove-select-type' => 'Seleccione o tipo',
+	'wikilove-get-started-header' => 'Comecemos!',
+	'wikilove-get-started-list-1' => 'Seleccione tipo de amor wiki que queira enviar',
+	'wikilove-get-started-list-2' => 'Engadir detalles ao seu amor wiki',
+	'wikilove-get-started-list-3' => 'Enviar o seu amor wiki!',
+	'wikilove-add-details' => 'Engadir detalles',
+	'wikilove-title' => 'Título:',
+	'wikilove-enter-message' => 'Escriba unha mensaxe:',
+	'wikilove-omit-sig' => '(sen sinatura)',
+	'wikilove-button-preview' => 'Vista previa',
+	'wikilove-preview' => 'Vista previa',
+	'wikilove-button-send' => 'Enviar o amor wiki',
+	'wikilove-type-makeyourown' => 'Faga o seu',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'wikilove-desc' => 'Adde un interfacie pro exprimer appreciation in le paginas de discussion de usatores',
+	'wikilove' => 'WikiLove',
+	'wikilove-enable-preference' => 'Permitter monstrar appreciation pro altere usatores con le scheda WikiLove (experimental)',
+	'wikilove-tab-text' => 'Monstrar appreciation',
+	'tooltip-ca-wikilove' => 'Publicar un message pro iste usator que monstra tu appreciation',
+	'wikilove-dialog-title' => 'WikiLove',
+	'wikilove-select-type' => 'Selige typo',
+	'wikilove-get-started-header' => 'Nos comencia!',
+	'wikilove-get-started-list-1' => 'Selige le typo de WikiLove que tu vole inviar',
+	'wikilove-get-started-list-2' => 'Adde detalios a tu WikiLove',
+	'wikilove-get-started-list-3' => 'Invia tu WikiLove!',
+	'wikilove-add-details' => 'Adder detalios',
+	'wikilove-title' => 'Titulo:',
+	'wikilove-enter-message' => 'Entra un message:',
+	'wikilove-omit-sig' => '(sin signatura)',
+	'wikilove-button-preview' => 'Previsualisar',
+	'wikilove-preview' => 'Previsualisation',
+	'wikilove-button-send' => 'Inviar WikiLove',
+	'wikilove-type-makeyourown' => 'Crear le tue',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'wikilove-desc' => 'Määd et müjjelesch, einem sing Aanäkännong ußzeshpräsche, övver däm sing Klaafsigg em Wiki.',
+	'wikilove' => 'Leev Wiki',
+	'wikilove-enable-preference' => 'Määd et müjjelesch, einem sing Aanäkännong ußzeshpräsche (för zom Ußprobeere)',
+	'wikilove-tab-text' => 'Donn Ding Aanerkennog ußdröcke',
+	'tooltip-ca-wikilove' => 'Donn Ding Aanerkennong en ene päsöönlesche Nohreesch ußdröcke',
+	'wikilove-dialog-title' => 'Aanäkännong em Wiki',
+	'wikilove-select-type' => 'Donn en Zoot ußwähle',
+	'wikilove-get-started-header' => 'Lom_mer ens med aanfange!',
+	'wikilove-get-started-list-1' => 'Donn de Aad udder Zoot vun Dinge Aanäkännong ußwähle, di de över et Wiki schecke wells',
+	'wikilove-get-started-list-2' => 'Donn Einzelheite övver Ding päsöönlesche Aanäkännong dobei',
+	'wikilove-get-started-list-3' => 'Donn Ding Aanäkännong övverjävve',
+	'wikilove-add-details' => 'Don noch Einzelheite dobei',
+	'wikilove-title' => 'Tittel:',
+	'wikilove-enter-message' => 'Jiv ene Täx en',
+	'wikilove-omit-sig' => '(der ohne en Ongerschreff)',
+	'wikilove-button-preview' => 'Vör-Aansesch',
+	'wikilove-preview' => 'Vör-Aansesch',
+	'wikilove-button-send' => 'Donn e joot Jeföhl schecke',
+	'wikilove-type-makeyourown' => 'Maach Ding eije',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'wikilove-get-started-header' => 'Elo geet et lass!',
+	'wikilove-add-details' => 'Detailer derbäisetzen',
+	'wikilove-title' => 'Titel:',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'wikilove-desc' => 'Додава посредник за искажување на позитивни мислења и поддршка на кориснички страници за разговор',
+	'wikilove' => 'ВикиЉубов',
+	'wikilove-enable-preference' => 'Овозможи оддавање на признанија и заслуги со јазичето ВикиЉубов (експериментално)',
+	'wikilove-tab-text' => 'Оддај признание',
+	'tooltip-ca-wikilove' => 'Оставете му порака на корисников кажувајќи му дека го цените',
+	'wikilove-dialog-title' => 'ВикиЉубов',
+	'wikilove-select-type' => 'Одберете тип',
+	'wikilove-get-started-header' => 'Да почнеме!',
+	'wikilove-get-started-list-1' => 'Одберете го типот на ВикиЉубов што сакате да ја испратите',
+	'wikilove-get-started-list-2' => 'Внесете содржина на ВикиЉубов-та',
+	'wikilove-get-started-list-3' => 'Испратете ја вашата ВикиЉубов',
+	'wikilove-add-details' => 'Внесете содржина',
+	'wikilove-title' => 'Наслов:',
+	'wikilove-enter-message' => 'Внесете порака:',
+	'wikilove-omit-sig' => '(без потпис)',
+	'wikilove-button-preview' => 'Преглед',
+	'wikilove-preview' => 'Преглед',
+	'wikilove-button-send' => 'Испрати ВикиЉубов',
+	'wikilove-type-makeyourown' => 'Направете своја',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'wikilove-desc' => "Voegt een interface toe voor het geven van positieve terugkoppeling op overlegpagina's van gebruikers",
+	'wikilove' => 'Wikiwaardering',
+	'wikilove-enable-preference' => 'Waardering geven aan andere gebruikers via Wikiwaardering inschakelen (experimenteel)',
+	'wikilove-tab-text' => 'Waardering tonen',
+	'tooltip-ca-wikilove' => 'Plaats een bericht voor deze gebruiker om uw waardering te tonen',
+	'wikilove-dialog-title' => 'Wikiwaardering',
+	'wikilove-select-type' => 'Selecteer type',
+	'wikilove-get-started-header' => 'Begin ermee!',
+	'wikilove-get-started-list-1' => 'Selecteer het type Wikiwaardering dat u wilt achterlaten',
+	'wikilove-get-started-list-2' => 'Voegt gegevens toe aan uw Wikiwaardering',
+	'wikilove-get-started-list-3' => 'Uw Wikiwaardering verzenden!',
+	'wikilove-add-details' => 'Gegevens toevoegen',
+	'wikilove-title' => 'Onderwerp:',
+	'wikilove-enter-message' => 'Voer een bericht in:',
+	'wikilove-omit-sig' => '(zonder ondertekening)',
+	'wikilove-button-preview' => 'Voorvertoning',
+	'wikilove-preview' => 'Voorvertoning',
+	'wikilove-button-send' => 'Wikiwaardering versturen',
+	'wikilove-type-makeyourown' => 'Uw eigen Wikiwaardering maken',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'wikilove-desc' => 'Adiciona uma interface para facilitar a colocação de demonstrações de apreço nas páginas de discussão dos utilizadores',
+	'wikilove' => 'WikiApreço',
+	'wikilove-enable-preference' => 'Permite enviar demonstrações de apreço a outros utilizadores usando o separador "WikiApreço" (experimental)',
+	'wikilove-tab-text' => 'Demonstrar apreço',
+	'tooltip-ca-wikilove' => 'Envie uma mensagem a este utilizador demonstrando o seu apreço',
+	'wikilove-dialog-title' => 'WikiApreço',
+	'wikilove-select-type' => 'Escolha o tipo',
+	'wikilove-get-started-header' => 'Vamos começar!',
+	'wikilove-get-started-list-1' => 'Escolha o tipo de WikiApreço que pretende enviar',
+	'wikilove-get-started-list-2' => 'Acrescente detalhes ao seu WikiApreço',
+	'wikilove-get-started-list-3' => 'Enviar o seu WikiApreço!',
+	'wikilove-add-details' => 'Adicionar detalhes',
+	'wikilove-title' => 'Título:',
+	'wikilove-enter-message' => 'Introduza uma mensagem:',
+	'wikilove-omit-sig' => '(sem assinatura)',
+	'wikilove-button-preview' => 'Antever',
+	'wikilove-preview' => 'Antevisão',
+	'wikilove-button-send' => 'Enviar WikiApreço',
+	'wikilove-type-makeyourown' => 'Crie o seu',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'wikilove-desc' => 'Nagdaragdag ng isang ugnayang-mukha para sa pagpapadali ng positibong puna ng tagagamit sa mga pahina ng usapan',
+	'wikilove' => 'WikiLove',
+	'wikilove-enable-preference' => 'Paganahin ang pagpapakita ng pagpapahalaga para sa iba pang mga tagagamit sa pamamagitan ng panglaylay na WikiLove (sinusubukan)',
+	'wikilove-tab-text' => 'Magpakita ng pagpapahalaga',
+	'tooltip-ca-wikilove' => 'Magpaskil ng isang mensahe para sa tagagamit na ito na nagpapakita ng pagpapahalaga mo',
+	'wikilove-dialog-title' => 'WikiLove',
+	'wikilove-select-type' => 'Piliin ang uri',
+	'wikilove-get-started-header' => 'Magsimula na tayo!',
+	'wikilove-get-started-list-1' => 'Piliin ang uri ng WikiLove na nais mong ipadala',
+	'wikilove-get-started-list-2' => 'Magdagdag ng mga detalye sa WikiLove mo',
+	'wikilove-get-started-list-3' => 'Ipadala ang WikiLove mo!',
+	'wikilove-add-details' => 'Magdagdag ng mga detalye',
+	'wikilove-title' => 'Pamagat:',
+	'wikilove-enter-message' => 'Magpasok ng isang mensahe:',
+	'wikilove-omit-sig' => '(walang lagda)',
+	'wikilove-button-preview' => 'Paunang tingin',
+	'wikilove-preview' => 'Paunang tingin',
+	'wikilove-button-send' => 'Ipadala ang WikiLove',
+	'wikilove-type-makeyourown' => 'Gumawa ng sarili mo',
+);
+
