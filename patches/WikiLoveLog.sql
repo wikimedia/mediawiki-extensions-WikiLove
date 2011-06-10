0bd1fdc758a95f20aec714087f0fd81a8ece2874
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-06-10 02:17:46 +0000

    adding new fields for requested analytics

--- patches/WikiLoveLog.sql
+++ patches/WikiLoveLog.sql
@@ -5,14 +5,20 @@
 --
 
 CREATE TABLE IF NOT EXISTS /*_*/wikilove_log (
-	wll_id int NOT NULL PRIMARY KEY auto_increment, -- unique id
-	wll_timestamp binary(14) NOT NULL,              -- timestamp
-	wll_sender int(11) NOT NULL,                    -- user id of the sender
-	wll_receiver int(11) NOT NULL,                  -- user id of the receiver
-	wll_type varchar(64) NOT NULL,                  -- type (and subtype) of message
-	wll_subject varchar(255) NOT NULL,              -- subject line
-	wll_message blob NOT NULL,                      -- actual message
-	wll_email bool NOT NULL default '0'             -- whether or not a notification mail has been sent
+	wll_id int NOT NULL PRIMARY KEY auto_increment,		-- unique id
+	wll_timestamp binary(14) NOT NULL,              	-- timestamp
+	wll_sender int(11) NOT NULL,                    	-- user id of the sender
+	wll_sender_registration varchar(32) default NULL,	-- registration date of the sender
+	wll_sender_totaledits mediumint(8) default NULL,	-- total number of edits for the sender
+	wll_sender_recentedits smallint(6) default NULL,	-- number of recent edits for the sender
+	wll_receiver int(11) NOT NULL,                  	-- user id of the receiver
+	wll_receiver_registration varchar(32) default NULL,	-- registration date of the receiver
+	wll_receiver_totaledits mediumint(8) default NULL,	-- total number of edits for the receiver
+	wll_receiver_recentedits smallint(6) default NULL,	-- number of recent edits for the receiver
+	wll_type varchar(64) NOT NULL,                  	-- type (and subtype) of message
+	wll_subject varchar(255) NOT NULL,              	-- subject line
+	wll_message blob NOT NULL,                      	-- actual message
+	wll_email bool NOT NULL default '0'             	-- whether or not a notification mail has been sent
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/wll_timestamp ON /*_*/wikilove_log (wll_timestamp);
