c807679018e0d4085008b342f3fc400242401512
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-10-13 20:38:05 +0000

    switching from GPL to MIT license per http://www.mediawiki.org/wiki/Thread:Extension_talk:WikiLove/Reusing_the_default_WikiLove.js, Jan Paul agrees with switch

--- WikiLove.php
+++ WikiLove.php
@@ -3,17 +3,17 @@
  * MediaWiki WikiLove extension
  * http://www.mediawiki.org/wiki/Extension:WikiLove
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * http://www.gnu.org/copyleft/gpl.html
+ * This program is distributed WITHOUT ANY WARRANTY.
  *
  * Heart icon by Mark James (Creative Commons Attribution 3.0 License)
  * Interface design by Brandon Harris
