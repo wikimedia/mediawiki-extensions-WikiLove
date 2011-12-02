277a6e11f06e9d4014fe3ee0584a1db097f32c24
Sam Reed <reedy@users.mediawiki.org>
2011-12-02 16:34:25 +0000

    Followup r104973, use those messages in this file
    I know, they still need exporting, but I've still not finished changing them over

--- modules/ext.wikiLove/ext.wikiLove.defaultOptions.js
+++ modules/ext.wikiLove/ext.wikiLove.defaultOptions.js
@@ -17,225 +17,225 @@ $.wikiLoveOptions = {
 		// example type, could be removed later
 		'barnstar': {
 			name: mw.msg( 'wikilove-type-barnstars' ), // name of the type (appears in the types menu)
-			select: 'Select a barnstar:', // subtype select label
+			select: mw.msg( 'wikilove-barnstar-select' ), // subtype select label
 			subtypes: { // some different subtypes
 				// note that when not using subtypes you should use these subtype options
 				// for the top-level type
 				'original': {
 					fields: [ 'message' ], // fields to ask for in form
-					option: 'Original Barnstar', // option listed in the select list
-					descr: 'This barnstar is given to recognize particularly fine contributions to Wikipedia, to let people know that their hard work is seen and appreciated.', // description
-					header: 'A barnstar for you!', // header that appears at the top of the talk page post (optional)
-					title: 'The Original Barnstar', // title that appears inside the award box (optional)
+					option: mw.msg( 'wikilove-barnstar-admin-' ), // option listed in the select list
+					descr: mw.msg( 'wikilove-barnstar-admin-' ), // description
+					header: mw.msg( 'wikilove-barnstar-header' ), // header that appears at the top of the talk page post (optional)
+					title: mw.msg( 'wikilove-barnstar-admin-' ), // title that appears inside the award box (optional)
 					image: 'Original Barnstar Hires.png' // image for the award
 				},
 				'admins': {
 					fields: [ 'message' ],
-					option: 'Admin\'s Barnstar',
-					descr: 'The Admin\'s Barnstar may be awarded to an administrator who made a particularly difficult decision or performed a tedious but needed admin task.',
-					header: 'A barnstar for you!',
-					title: 'The Admin\'s Barnstar',
+					option: mw.msg( 'wikilove-barnstar-admins-option' ),
+					descr: mw.msg( 'wikilove-barnstar-admins-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-admins-title' ),
 					image: 'Administrator Barnstar Hires.png'
 				},
 				'antivandalism': {
 					fields: [ 'message' ],
-					option: 'Anti-Vandalism Barnstar',
-					descr: 'The Anti-Vandalism Barnstar may be awarded to those who show great contributions to protecting and reverting attacks of vandalism on Wikipedia.',
-					header: 'A barnstar for you!',
-					title: 'The Anti-Vandalism Barnstar',
+					option: mw.msg( 'wikilove-barnstar-antivandalism-option' ),
+					descr: mw.msg( 'wikilove-barnstar-antivandalism-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-antivandalism-title' ),
 					image: 'Barnstar of Reversion Hires.png'
 				},
 				'diligence': {
 					fields: [ 'message' ],
-					option: 'Barnstar of Diligence',
-					descr: 'The Barnstar of Diligence may be awarded in recognition of a combination of extraordinary scrutiny, precision and community service.',
-					header: 'A barnstar for you!',
-					title: 'The Barnstar of Diligence',
+					option: mw.msg( 'wikilove-barnstar-diligence-option' ),
+					descr: mw.msg( 'wikilove-barnstar-diligence-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-diligence-title' ),
 					image: 'Barnstar of Diligence Hires.png'
 				},
 				'diplomacy': {
 					fields: [ 'message' ],
-					option: 'Barnstar of Diplomacy',
-					descr: 'The Barnstar of Diplomacy is awarded to users who have helped to resolve, peacefully, conflicts on Wikipedia.',
-					header: 'A barnstar for you!',
-					title: 'The Barnstar of Diplomacy',
+					option: mw.msg( 'wikilove-barnstar-diplomacy-option' ),
+					descr: mw.msg( 'wikilove-barnstar-diplomacy-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-diplomacy-title' ),
 					image: 'Peace Barnstar Hires.png'
 				},
 				'goodhumor': {
 					fields: [ 'message' ],
-					option: 'Barnstar of Good Humor',
-					descr: 'The Barnstar of Good Humor may be awarded to Wikipedians who consistently lighten the mood, defuse conflicts, and make Wikipedia a better place to be.',
-					header: 'A barnstar for you!',
-					title: 'The Barnstar of Good Humor',
+					option: mw.msg( 'wikilove-barnstar-goodhumor-option' ),
+					descr: mw.msg( 'wikilove-barnstar-goodhumor-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-goodhumor-title' ),
 					image: 'Barnstar of Humour Hires.png'
 				},
 				'brilliant': {
 					fields: [ 'message' ],
-					option: 'Brilliant Idea Barnstar',
-					descr: 'The Brilliant Idea Barnstar may be awarded to a user who figures out an elegant solution to a particularly difficult problem.',
-					header: 'A barnstar for you!',
-					title: 'The Brilliant Idea Barnstar',
+					option: mw.msg( 'wikilove-barnstar-brilliant-option' ),
+					descr: mw.msg( 'wikilove-barnstar-brilliant-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-brilliant-title' ),
 					image: 'Brilliant Idea Barnstar Hires.png'
 				},
 				'citation': {
 					fields: [ 'message' ],
-					option: 'Citation Barnstar',
-					descr: 'The Citation Barnstar is awarded to users who provide references and in-line citations to previously unsourced articles.',
-					header: 'A barnstar for you!',
-					title: 'The Citation Barnstar',
+					option: mw.msg( 'wikilove-barnstar-citation-option' ),
+					descr: mw.msg( 'wikilove-barnstar-citation-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-citation-title' ),
 					image: 'Citation Barnstar Hires.png'
 				},
 				'civility': {
 					fields: [ 'message' ],
-					option: 'Civility Barnstar',
-					descr: 'The Civility Barnstar may be awarded to any user who excels at maintaining civility in the midst of contentious situations.',
-					header: 'A barnstar for you!',
-					title: 'The Civility Barnstar',
+					option: mw.msg( 'wikilove-barnstar-civility-option' ),
+					descr: mw.msg( 'wikilove-barnstar-civility-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-civility-title' ),
 					image: 'Civility Barnstar Hires.png'
 				},
 				'copyeditor': {
 					fields: [ 'message' ],
-					option: 'Copyeditor\'s Barnstar',
-					descr: 'The Copyeditor\'s Barnstar is awarded for excellence in correcting spelling, grammar, punctuation, and style issues.',
-					header: 'A barnstar for you!',
-					title: 'The Copyeditor\'s Barnstar',
+					option: mw.msg( 'wikilove-barnstar-copyeditor-option' ),
+					descr: mw.msg( 'wikilove-barnstar-copyeditor-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-copyeditor-title' ),
 					image: 'Copyeditor Barnstar Hires.png'
 				},
 				'defender': {
 					fields: [ 'message' ],
-					option: 'Defender of the Wiki Barnstar',
-					descr: 'The Defender of the Wiki may be awarded to those who have gone above and beyond to prevent Wikipedia from being used for fraudulent purposes.',
-					header: 'A barnstar for you!',
-					title: 'The Defender of the Wiki Barnstar',
+					option: mw.msg( 'wikilove-barnstar-defender-option' ),
+					descr: mw.msg( 'wikilove-barnstar-defender-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-defender-title' ),
 					image: 'WikiDefender Barnstar Hires.png'
 				},
 				'editors': {
 					fields: [ 'message' ],
-					option: 'Editor\'s Barnstar',
-					descr: 'The Editor\'s Barnstar is awarded to individuals who display particularly fine decisions in general editing.',
-					header: 'A barnstar for you!',
-					title: 'The Editor\'s Barnstar',
+					option: mw.msg( 'wikilove-barnstar-editors-option' ),
+					descr: mw.msg( 'wikilove-barnstar-editors-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-editors-title' ),
 					image: 'Editors Barnstar Hires.png'
 				},
 				'designers': {
 					fields: [ 'message' ],
-					option: 'Graphic Designer\'s Barnstar',
-					descr: 'The Graphic Designer\'s Barnstar may be awarded to those who work tirelessly to provide Wikipedia with free, high-quality graphics.',
-					header: 'A barnstar for you!',
-					title: 'The Graphic Designer\'s Barnstar',
+					option: mw.msg( 'wikilove-barnstar-designers-option' ),
+					descr: mw.msg( 'wikilove-barnstar-designers-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-designers-title' ),
 					image: 'Graphic Designer Barnstar Hires.png'
 				},
 				'half': {
 					fields: [ 'message' ],
-					option: 'Half Barnstar',
-					descr: 'The Half Barnstar is awarded for excellence in cooperation, especially for productive editing with someone who holds an opposing viewpoint.',
-					header: 'A barnstar for you!',
-					title: 'The Half Barnstar',
+					option: mw.msg( 'wikilove-barnstar-half-option' ),
+					descr: mw.msg( 'wikilove-barnstar-half-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-half-title' ),
 					image: 'Halfstar Hires.png',
 					imageSize: '60px'
 				},
 				'minor': {
 					fields: [ 'message' ],
-					option: 'Minor Barnstar',
-					descr: 'Minor edits are often overlooked, but are essential contributions to Wikipedia. The Minor Barnstar is awarded for making minor edits of the utmost quality.',
-					header: 'A barnstar for you!',
-					title: 'The Minor barnstar',
+					option: mw.msg( 'wikilove-barnstar-minor-option' ),
+					descr: mw.msg( 'wikilove-barnstar-minor-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-minor-title' ),
 					image: 'Minor Barnstar Hires.png'
 				},
 				'antispam': {
 					fields: [ 'message' ],
-					option: 'No Spam Barnstar',
-					descr: 'The Anti-Spam Barnstar is awarded to users who do an exceptional job fighting against spam on Wikipedia.',
-					header: 'A barnstar for you!',
-					title: 'The No Spam Barnstar',
+					option: mw.msg( 'wikilove-barnstar-antispam-option' ),
+					descr: mw.msg( 'wikilove-barnstar-antispam-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-antispam-title' ),
 					image: 'No Spam Barnstar Hires.png'
 				},
 				'photographers': {
 					fields: [ 'message' ],
-					option: 'Photographer\'s Barnstar',
-					descr: 'The Photographer\'s Barnstar is awarded to those individuals who tirelessly improve the Wikipedia with their photographic skills and contributions.',
-					header: 'A barnstar for you!',
-					title: 'The Photographer\'s Barnstar',
+					option: mw.msg( 'wikilove-barnstar-photographers-option' ),
+					descr: mw.msg( 'wikilove-barnstar-photographers-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-photographers-title' ),
 					image: 'Camera Barnstar Hires.png'
 				},
 				'kindness': {
 					fields: [ 'message' ],
-					option: 'Random Acts of Kindness Barnstar',
-					descr: 'The Random Acts of Kindness Barnstar may be awarded to those that show a pattern of going the extra mile to be nice, without being asked.',
-					header: 'A barnstar for you!',
-					title: 'The Random Acts of Kindness Barnstar',
+					option: mw.msg( 'wikilove-barnstar-kindness-option' ),
+					descr: mw.msg( 'wikilove-barnstar-kindness-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-kindness-title' ),
 					image: 'Kindness Barnstar Hires.png'
 				},
 				'reallife': {
 					fields: [ 'message' ],
-					option: 'Real Life Barnstar',
-					descr: 'The Real Life Barnstar is awarded to editors who make contributions both online and offline, by organizing wiki-related real-life events.',
-					header: 'A barnstar for you!',
-					title: 'The Real Life Barnstar',
+					option: mw.msg( 'wikilove-barnstar-reallife-option' ),
+					descr: mw.msg( 'wikilove-barnstar-reallife-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-reallife-title' ),
 					image: 'Real Life Barnstar.jpg'
 				},
 				'resilient': {
 					fields: [ 'message' ],
-					option: 'Resilient Barnstar',
-					descr: 'The Resilient Barnstar may be given to any editor who learns and improves from criticisms, never letting mistakes impede their growth as a Wikipedian.',
-					header: 'A barnstar for you!',
-					title: 'The Resilient Barnstar',
+					option: mw.msg( 'wikilove-barnstar-resilient-option' ),
+					descr: mw.msg( 'wikilove-barnstar-resilient-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-resilient-title' ),
 					image: 'Resilient Barnstar Hires.png'
 				},
 				'rosetta': {
 					fields: [ 'message' ],
-					option: 'Rosetta Barnstar',
-					descr: 'The Rosetta Barnstar may be given to any editor who exhibits outstanding translation efforts on Wikipedia.',
-					header: 'A barnstar for you!',
-					title: 'The Rosetta Barnstar',
+					option: mw.msg( 'wikilove-barnstar-rosetta-option' ),
+					descr: mw.msg( 'wikilove-barnstar-rosetta-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-rosetta-title' ),
 					image: 'Rosetta Barnstar Hires.png'
 				},
 				'special': {
 					fields: [ 'message' ],
-					option: 'Special Barnstar',
-					descr: 'The Special Barnstar may be awarded to a user as a gesture of appreciation when there is no other barnstar which would be appropriate.',
-					header: 'A barnstar for you!',
-					title: 'The Special Barnstar',
+					option: mw.msg( 'wikilove-barnstar-special-option' ),
+					descr: mw.msg( 'wikilove-barnstar-special-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-special-title' ),
 					image: 'Special Barnstar Hires.png'
 				},
 				'surreal': {
 					fields: [ 'message' ],
-					option: 'Surreal Barnstar',
-					descr: 'The Surreal Barnstar may be awarded to any Wikipedian who adds "special flavor" to the community by acting as a sort of wildcard.',
-					header: 'A barnstar for you!',
-					title: 'The Surreal Barnstar',
+					option: mw.msg( 'wikilove-barnstar-surreal-option' ),
+					descr: mw.msg( 'wikilove-barnstar-surreal-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-surreal-title' ),
 					image: 'Surreal Barnstar Hires.png'
 				},
 				'teamwork': {
 					fields: [ 'message' ],
-					option: 'Teamwork Barnstar',
-					descr: 'The Teamwork Barnstar may be awarded when several editors work together to improve an article.',
-					header: 'A barnstar for you!',
-					title: 'The Teamwork Barnstar',
+					option: mw.msg( 'wikilove-barnstar-teamwork-option' ),
+					descr: mw.msg( 'wikilove-barnstar-teamwork-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-teamwork-title' ),
 					image: 'Team Barnstar Hires.png'
 				},
 				'technical': {
 					fields: [ 'message' ],
-					option: 'Technical Barnstar',
-					descr: 'The Technical Barnstar may be awarded to anyone who has enhanced Wikipedia through their technical work (programming, bot building, link repair, etc.).',
-					header: 'A barnstar for you!',
-					title: 'The Technical Barnstar',
+					option: mw.msg( 'wikilove-barnstar-technical-option' ),
+					descr: mw.msg( 'wikilove-barnstar-technical-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-technical-title' ),
 					image: 'Vitruvian Barnstar Hires.png'
 				},
 				'tireless': {
 					fields: [ 'message' ],
-					option: 'Tireless Contributor Barnstar',
-					descr: 'The Tireless Contributor Barnstar is awarded to especially tireless Wikipedians who contribute an especially large body of work without sacrificing quality.',
-					header: 'A barnstar for you!',
-					title: 'The Tireless Contributor Barnstar',
+					option: mw.msg( 'wikilove-barnstar-tireless-option' ),
+					descr: mw.msg( 'wikilove-barnstar-tireless-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-tireless-title' ),
 					image: 'Tireless Contributor Barnstar Hires.gif'
 				},
 				'writers': {
 					fields: [ 'message' ],
-					option: 'Writer\'s Barnstar',
-					descr: 'The Writer\'s Barnstar may be awarded to any user who has written a large number of articles or has contributed a large number of edits.',
-					header: 'A barnstar for you!',
-					title: 'The Writer\'s Barnstar',
+					option: mw.msg( 'wikilove-barnstar-writers-option' ),
+					descr: mw.msg( 'wikilove-barnstar-writers-desc' ),
+					header: mw.msg( 'wikilove-barnstar-header' ),
+					title: mw.msg( 'wikilove-barnstar-writers-title' ),
 					image: 'Writers Barnstar Hires.png'
 				}
 			},
@@ -243,7 +243,7 @@ $.wikiLoveOptions = {
 		},
 		'food': {
 			name: mw.msg( 'wikilove-type-food' ), // name of the type (appears in the types menu)
-			select: 'Select food or drink item:', // subtype select label
+			select: mw.msg( 'wikilove-food-select' ), // subtype select label
 			text: '{| style="background-color: $5; border: 1px solid $6;"\n\
 |style="vertical-align: middle; padding: 5px;" | [[$3|$4]]\n\
 |style="vertical-align: middle; padding: 3px;" | $1 ~~~~\n\
@@ -353,7 +353,7 @@ $.wikiLoveOptions = {
 		'kitten': {
 			name: mw.msg( 'wikilove-type-kittens' ),
 			fields: [ 'header', 'message' ],
-			header: 'A kitten for you!',
+			header: mw.msg( 'wikilove-kittens-header' ),
 			text: '[[$3|left|150px]]\n$1\n\n~~~~\n<br style="clear: both"/>', // $3 is the image filename
 			gallery: {
 				imageList: [ 'Cucciolo gatto Bibo.jpg', 'Kitten (06) by Ron.jpg', 'Kitten-stare.jpg', 'Red Kitten 01.jpg', 'Kitten in a helmet.jpg', 'Cute grey kitten.jpg' ],
