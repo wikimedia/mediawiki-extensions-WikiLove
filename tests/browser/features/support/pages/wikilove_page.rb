21d8ce1623a703a45b296f5657d1346dec26c4ef
Željko Filipin <zeljko.filipin@gmail.com>
2015-08-28 16:18:57 +0200

    Fixed Style/StringLiterals RuboCop offense
    Bug: T110672
    Change-Id: I552e8c7ad4d0b377046ba47f9f5ed5a37e02d041

--- tests/browser/features/support/pages/wikilove_page.rb
+++ tests/browser/features/support/pages/wikilove_page.rb
@@ -9,23 +9,23 @@
 # WikiLove top-level directory and at
 # https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FWikiLove/HEAD/CREDITS
 #
-require "page-object"
+require 'page-object'
 
 class WikilovePage
   include PageObject
 
-  page_url "User:Selenium_user2"
+  page_url 'User:Selenium_user2'
 
-  select_list(:barnstar_select, id: "mw-wikilove-subtype")
-  span(:barnstars, text: "Barnstars")
-  span(:food_and_drink, text: "Food and drink")
-  select_list(:food_drink_select, id: "mw-wikilove-subtype")
-  text_field(:food_header, id: "mw-wikilove-header")
-  a(:heart, text: "WikiLove")
-  text_field(:kitten_header, id: "mw-wikilove-header")
-  a(:kitten_image, id: "mw-wikilove-gallery-img-0")
-  span(:kittens, text: "Kittens")
-  text_area(:wikilove_message, id: "mw-wikilove-message")
-  div(:wikilove_spinner, id: "mw-wikilove-image-preview-spinner")
-  div(:wikilove_window, class: "ui-dialog ui-widget ui-widget-content ui-corner-all ui-draggable")
+  select_list(:barnstar_select, id: 'mw-wikilove-subtype')
+  span(:barnstars, text: 'Barnstars')
+  span(:food_and_drink, text: 'Food and drink')
+  select_list(:food_drink_select, id: 'mw-wikilove-subtype')
+  text_field(:food_header, id: 'mw-wikilove-header')
+  a(:heart, text: 'WikiLove')
+  text_field(:kitten_header, id: 'mw-wikilove-header')
+  a(:kitten_image, id: 'mw-wikilove-gallery-img-0')
+  span(:kittens, text: 'Kittens')
+  text_area(:wikilove_message, id: 'mw-wikilove-message')
+  div(:wikilove_spinner, id: 'mw-wikilove-image-preview-spinner')
+  div(:wikilove_window, class: 'ui-dialog ui-widget ui-widget-content ui-corner-all ui-draggable')
 end
