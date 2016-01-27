7d61821685181b8c97a4df9d988495ab25a3b04f
Kaldari <rkaldari@wikimedia.org>
2016-01-26 19:25:03 -0600

    Adjusting schema to match user_id definition in core
    Not including an update patch since the change has almost no practical
    effect on existing installations.
    
    Also fixing some whitespace issues.
    
    Change-Id: If795591b18a385657a66a19068ea54b4b9c6523d

--- patches/WikiLoveLog.sql
+++ patches/WikiLoveLog.sql
@@ -5,18 +5,18 @@
 --
 
 CREATE TABLE IF NOT EXISTS /*_*/wikilove_log (
-	wll_id int NOT NULL PRIMARY KEY auto_increment,		-- unique id
-	wll_timestamp binary(14) NOT NULL,              	-- timestamp
-	wll_sender int(11) NOT NULL,                    	-- user id of the sender
-	wll_sender_registration binary(14) default NULL,	-- registration date of the sender
-	wll_sender_editcount int(11) default NULL,			-- total number of edits for the sender
-	wll_receiver int(11) NOT NULL,                  	-- user id of the receiver
-	wll_receiver_registration binary(14) default NULL,	-- registration date of the receiver
-	wll_receiver_editcount int(11) default NULL,		-- total number of edits for the receiver
-	wll_type varchar(64) NOT NULL,                  	-- type (and subtype) of message
-	wll_subject varchar(255) NOT NULL,              	-- subject line
-	wll_message blob NOT NULL,                      	-- actual message
-	wll_email bool NOT NULL default '0'             	-- whether or not a notification mail has been sent
+	wll_id int NOT NULL PRIMARY KEY auto_increment,     -- unique id
+	wll_timestamp binary(14) NOT NULL,                  -- timestamp
+	wll_sender int unsigned NOT NULL,                   -- user id of the sender
+	wll_sender_registration binary(14) default NULL,    -- registration date of the sender
+	wll_sender_editcount int(11) default NULL,          -- total number of edits for the sender
+	wll_receiver int unsigned NOT NULL,                 -- user id of the receiver
+	wll_receiver_registration binary(14) default NULL,  -- registration date of the receiver
+	wll_receiver_editcount int(11) default NULL,        -- total number of edits for the receiver
+	wll_type varchar(64) NOT NULL,                      -- type (and subtype) of message
+	wll_subject varchar(255) NOT NULL,                  -- subject line
+	wll_message blob NOT NULL,                          -- actual message
+	wll_email bool NOT NULL default '0'                 -- whether or not a notification mail has been sent
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/wll_timestamp ON /*_*/wikilove_log (wll_timestamp);
