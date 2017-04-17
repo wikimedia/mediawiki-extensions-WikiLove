0b18b6193edcd971e9591df6e4ac414f09d8759d
Zach <terracodes@tools.wmflabs.org>
2017-04-17 14:04:20 -0800

    git.wikimedia.org -> phab
    Bug: T139089
    Change-Id: Ia3cdf1105b0ba1084610c7ff971d984f31d23982

--- tests/browser/features/step_definitions/wikilove_steps.rb
+++ tests/browser/features/step_definitions/wikilove_steps.rb
@@ -1,13 +1,13 @@
 #
 # This file is subject to the license terms in the COPYING file found in the
 # WikiLove top-level directory and at
-# https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FWikiLove/HEAD/COPYING. No part of
+# https://phabricator.wikimedia.org/diffusion/EWLO/browse/master/COPYING. No part of
 # WikiLove, including this file, may be copied, modified, propagated, or
 # distributed except according to the terms contained in the COPYING file.
 #
 # Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
 # WikiLove top-level directory and at
-# https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FWikiLove/HEAD/CREDITS
+# https://phabricator.wikimedia.org/diffusion/EWLO/browse/master/CREDITS
 #
 When(/^I click Barnstars$/) do
   on(WikilovePage).barnstars_element.click
