20b7dfa3f8972bdf3fba4d3d86388424efab9beb
Umherirrender <umherirrender_de.wp@web.de>
2019-05-16 20:28:09 +0200

    Add phan
    Change-Id: I5c44fd992c298fae7637c142b981b6a17e8c061a

---
+++ .phan/config.php
@@ -0,0 +1,20 @@
+<?php
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/Flow',
+		'../../extensions/LiquidThreads',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/Flow',
+		'../../extensions/LiquidThreads',
+	]
+);
+
+return $cfg;
