d9d672d744eb86c8d975907ffbea00a122bcdff7
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-12-18 19:31:11 +0000

    build: Updating mediawiki/minus-x to 0.3.2
    Additional changes:
    * Added .eslintcache to .gitignore.
    * Removing manual reportUnusedDisableDirectives for eslint.
    
    Change-Id: Ic92821efc5793dd12dda9c80351f37d9b80036b8

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,8 +17,7 @@ module.exports = function ( grunt ) {
 		eslint: {
 			options: {
 				extensions: [ '.js', '.json' ],
-				cache: true,
-				reportUnusedDisableDirectives: true
+				cache: true
 			},
 			all: [
 				'**/*.{js,json}',
