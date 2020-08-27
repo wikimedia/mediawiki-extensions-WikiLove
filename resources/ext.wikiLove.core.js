e877280033310fbea2e726086cdc0baf8e2b5cfd
Ed Sanders <esanders@wikimedia.org>
2020-08-27 22:47:00 +0100

    eslint cleanup
    Change-Id: I7caa6ae32a83d766fb3f5ec40bf7018fc8e3f334

--- resources/ext.wikiLove.core.js
+++ resources/ext.wikiLove.core.js
@@ -525,10 +525,12 @@
 		},
 
 		showAddDetailsError: function ( errmsg ) {
+			// eslint-disable-next-line mediawiki/msg-doc
 			$( '#mw-wikilove-add-details' ).append( $( '<div>' ).addClass( 'mw-wikilove-error' ).text( mw.msg( errmsg ) ) );
 		},
 
 		showPreviewError: function ( errmsg ) {
+			// eslint-disable-next-line mediawiki/msg-doc
 			$( '#mw-wikilove-preview' ).append( $( '<div>' ).addClass( 'mw-wikilove-error' ).text( mw.msg( errmsg ) ) );
 		},
 
