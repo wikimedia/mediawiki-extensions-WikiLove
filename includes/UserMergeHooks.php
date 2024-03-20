9f616d11b4f5365a4ebb8418fe4a2c04d543ab92
Fomafix <fomafix@googlemail.com>
2024-03-20 19:54:05 +0000

    Replace global configuration variables by MainConfig
    Change-Id: I91a81aa54ea2f28eac520dcf0dc86eb3630d77f5

--- includes/UserMergeHooks.php
+++ includes/UserMergeHooks.php
@@ -4,6 +4,7 @@
 
 namespace MediaWiki\Extension\WikiLove;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Extension\UserMerge\Hooks\AccountFieldsHook;
 use Wikimedia\Rdbms\ILoadBalancer;
 
@@ -15,14 +16,18 @@ use Wikimedia\Rdbms\ILoadBalancer;
  */
 
 class UserMergeHooks implements AccountFieldsHook {
+	private Config $config;
 	private ILoadBalancer $loadBalancer;
 
 	/**
+	 * @param Config $config
 	 * @param ILoadBalancer $loadBalancer
 	 */
 	public function __construct(
+		Config $config,
 		ILoadBalancer $loadBalancer
 	) {
+		$this->config = $config;
 		$this->loadBalancer = $loadBalancer;
 	}
 
@@ -32,11 +37,13 @@ class UserMergeHooks implements AccountFieldsHook {
 	 * @param array &$updateFields
 	 */
 	public function onUserMergeAccountFields( array &$updateFields ): void {
-		global $wgWikiLoveLogging;
 		$dbr = $this->loadBalancer->getMaintenanceConnectionRef( DB_REPLICA );
 		// FIXME HACK: The extension never actually required the 'wikilove_log' table
 		// and would suppress db errors if it didn't exist
-		if ( $wgWikiLoveLogging && $dbr->tableExists( 'wikilove_log', __METHOD__ ) ) {
+		if (
+			$this->config->get( 'WikiLoveLogging' ) &&
+			$dbr->tableExists( 'wikilove_log', __METHOD__ )
+		) {
 			$updateFields[] = [ 'wikilove_log', 'wll_sender' ];
 			$updateFields[] = [ 'wikilove_log', 'wll_receiver' ];
 		}
